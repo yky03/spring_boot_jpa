74db56bb0f39f31a1fc3d5d1f3123d06dfbb774b
양기열 <ykycome00@naver.com>
2020-08-30 18:03:14 +0900

    원복1

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -3,7 +3,6 @@ package com.example.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//브랜치 병합 테스트
 @SpringBootApplication
 public class TestApplication {
 
